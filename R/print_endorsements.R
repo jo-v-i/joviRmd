1d553e5ffae82b4e8d78628145bc3c18c8df0965
Matthew Kay <matthew.kay@gmail.com>
2020-05-11 15:24:56 -0400

    add git version comparison to endorsements

--- R/print_endorsements.R
+++ R/print_endorsements.R
@@ -4,6 +4,7 @@
 #' @export
 print_endorsements = function() {
   endorsements = rmarkdown::metadata$endorsements
+  repo_url = rmarkdown::metadata$repository_url
 
   if (length(endorsements) == 0) {
     cat('<p><small>This paper has not yet received any endorsements</small></p><hr>')
@@ -17,7 +18,11 @@ print_endorsements = function() {
   for (e in endorsements) {
     name = if (is.null(e$name)) "" else e$name
     link = if (is.null(e$url)) name else paste0('<a href="', e$url, '">', name, '</a>')
-    date = if (is.null(e$date)) "" else paste0('(as of ', e$date, ')')
-    cat('<p>', link, date, "</p>")
+    date = if (is.null(e$date)) "" else paste0('on ', e$date, '')
+    version = if (is.null(repo_url) || is.null(e$commit)) "" else paste0(
+      '| version <a href="', repo_url, '/tree/', e$commit, '">', substr(e$commit, 1, 7), '</a> | ',
+      '<a href="', repo_url, '/compare/', e$commit, '...master', '">compare</a>'
+    )
+    cat('<p>', link, date, version, "</p>")
   }
 }
